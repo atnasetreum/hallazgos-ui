82b697b8acf0ac08270424457122178de37b3618
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2024-04-12 10:57:49 -0600

    feat: add service worker

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,16 @@ import ThemeRegistry from "ThemeRegistry";
 export const metadata: Metadata = {
   title: "HADA",
   description: "Hada ComportArte",
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+  ],
 };
 
 import "@fontsource/roboto/300.css";

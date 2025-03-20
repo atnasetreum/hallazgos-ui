8e57e1c8ea6a6f74c2559c29d29266885598916d
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2025-03-20 09:51:42 -0600

    refactor: set input password

--- app/_components/login/FormLogin.tsx
+++ app/_components/login/FormLogin.tsx
@@ -7,12 +7,33 @@ import { useRouter } from "next/navigation";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 import { AuthService } from "@services";
 import { constants } from "@constants";
 import { notify, isValidEmail } from "@shared/utils";
 
 const FormLogin = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+  };
+
   const [email, setEmail] = useState(
     process.env.NODE_ENV === constants.environments.development
       ? "eduardo-266@hotmail.com"
@@ -23,6 +44,8 @@ const FormLogin = () => {
   );
 
   const router = useRouter();
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
 
   const login = async () => {
     const emailClear = email.trim();
@@ -63,15 +86,34 @@ const FormLogin = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <TextField
-        margin="normal"
-        fullWidth
-        label="Contraseña"
-        type="password"
-        autoComplete="current-password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <FormControl sx={{ width: "100%" }} variant="outlined">
+        <InputLabel htmlFor="outlined-adornment-password">
+          Contraseña
+        </InputLabel>
+        <OutlinedInput
+          id="outlined-adornment-password"
+          type={showPassword ? "text" : "password"}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          autoComplete="current-password"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label={
+                  showPassword ? "hide the password" : "display the password"
+                }
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                onMouseUp={handleMouseUpPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label="Password"
+        />
+      </FormControl>
       <Button
         fullWidth
         variant="contained"

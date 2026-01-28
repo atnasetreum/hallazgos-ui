2e4ab680e14912a57213f45c4db1516283d35493
Eduardo San Miguel Domínguez García <atnasetreum@gmail.com>
2026-01-27 22:11:54 -0600

    feat: create GT done

--- app/_shared/components/menu.tsx
+++ app/_shared/components/menu.tsx
@@ -160,15 +160,15 @@ export const MainListItems = () => {
               />
               <CreateLink
                 nested
-                url="/processes"
-                title="Procesos"
-                icon={<AccountTreeIcon />}
-              />
-              <CreateLink
-                nested
                 url="/topics-tg"
                 title="Temas - G. Entr."
                 icon={<ContentPasteGoIcon />}
+              />
+              <CreateLink
+                nested
+                url="/processes"
+                title="Procesos2"
+                icon={<AccountTreeIcon />}
               />
               <CreateLink
                 nested
